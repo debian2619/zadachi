7e64656638d8e0a44f9c8fb9642892499f52dc53
Dmitry <ya.potapov@yandex.ru>
2022-07-19 18:59:27 +0300

    created zadacha2

---
+++ zadacha2/Program.cs
@@ -0,0 +1,21 @@
+﻿Console.WriteLine("Put number x : ");
+int x = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Put number y : ");
+int y = Convert.ToInt32(Console.ReadLine());
+if(x > 0 && y > 0)
+{
+    Console.WriteLine(" First guarter ");
+}
+if(x > 0 && y < 0)
+{
+    Console.WriteLine(" Fourth guarter ");
+}
+if(x < 0 && y < 0)
+{
+    Console.WriteLine(" Third quarter ");
+}
+if(x < 0 && y > 0)
+{
+    Console.WriteLine(" Second quarter ");
+}
+
