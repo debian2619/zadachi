cf78654360b04fea730ee76aec32215587dcdf7a
Dmitry <ya.potapov@yandex.ru>
2022-07-20 15:36:00 +0300

    created zadacha3

---
+++ Exersise3/Program.cs
@@ -0,0 +1,22 @@
+﻿Console.WriteLine("Put number a : ");
+int a = Convert.ToInt32(Console.ReadLine());
+
+void Diapazon(int a)
+{
+if(a == 1)
+
+    Console.WriteLine(" First guarter ");
+
+if(a == 2)
+
+    Console.WriteLine(" Second guarter ");
+
+if(a == 3)
+
+    Console.WriteLine(" Third quarter ");
+
+if(a == 4)
+
+    Console.WriteLine(" Forth quarter ");
+}
+Diapazon(a);
