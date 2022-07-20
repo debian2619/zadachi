19ceb392aaa4d055efeee727f2c0396b1a23643c
Dmitry <ya.potapov@yandex.ru>
2022-07-20 16:16:58 +0300

    created zadacha4

---
+++ zadacha4/Program.cs
@@ -0,0 +1,16 @@
+﻿Console.WriteLine("task 22 ");
+Console.WriteLine("Put number a : ");
+int N = Convert.ToInt32(Console.ReadLine());
+int sum = 0;
+int count = 1;
+while(count <= N)
+{
+    Console.Write(count*count);
+    Console.Write(", ");
+    count++;
+}
+Console.WriteLine(sum);
+
+
+
+
