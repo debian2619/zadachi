6ac80f6e3001f948a6df92102549dd00f0ffbeca
Dmitry <ya.potapov@yandex.ru>
2022-07-20 18:35:03 +0300

    created zadacha6

---
+++ zadacha6/Program.cs
@@ -0,0 +1,24 @@
+﻿Console.Write("Put number : ");
+int N = Convert.ToInt32(Console.ReadLine());
+void PrintArray(int[] col)
+{
+    int count = col.Length;
+    int position = 0;
+    while(position < count)
+{
+        Console.WriteLine(col[position]);
+        position++;
+}
+
+               
+}
+int[] array = new int[N];
+array[0] = 0;
+array[1] = 1;
+int index = 2;
+while(index < N)
+{
+    array[index] = array[index-1] + array[index-2];
+    index++;
+}
+PrintArray(array);
